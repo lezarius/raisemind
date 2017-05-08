5a24744a544e5bf8d3fac4975c02b76c91810c31
lezarius <remy.mazabraud@gmail.com>
2017-05-08 18:43:34 +0200

    achange extension into .php

---
+++ footer.php
@@ -0,0 +1,20 @@
+
+			<!-- Footer -->
+				<footer id="footer">
+					<div class="inner">
+						<div class="flex">
+							<div class="copyright">
+								&copy; <a href="http://raisemind.pw">RaiseMind.</a> Design: RaiseMind. Logo: <a href="https://twitter.com/SoniopArt">Soniop</a>.
+							</div>
+							<ul class="icons">
+								<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+							</ul>
+						</div>
+					</div>
+				</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
---
+++ index.php
@@ -0,0 +1,151 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>RaiseMind</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header">
+				<div class="inner">
+					<a href="http://dev.infra-rm.xyz/" class="logo"><img src="images/logo100.png" alt=""></img></a>
+					<nav id="nav">
+						<a href="index.html">Home</a>
+						<a href="politique.html">Notre politique</a>
+						<a href="#">Gestion de projets</a>
+						<a href="#">Nos services</a>
+						<a href="#">Notre équipe</a>
+						<a href="#">Blog</a>
+						<a href="#">Contact</a>
+					<!--
+					<a href="generic.html">Generic</a>
+						<a href="elements.html">Elements</a>
+					-->
+					</nav>
+					<a href="#navPanel" class="navPanelToggle"><span class="fa fa-bars"></span></a>
+				</div>
+			</header>
+
+		<!-- Banner -->
+			<section id="banner">
+				<h1>Bienvenue sur RaiseMind</h1>
+				<p>Une entreprise qui se veut être là pour repenser, les services web de demain.</p>
+			</section>
+
+		<!-- One -->
+			<section id="one" class="wrapper">
+				<div class="inner">
+					<div class="flex flex-3">
+						<article>
+							<header>
+								<h3>Notre politique libriste.<br /> Une politique pour vous !</h3>
+							</header>
+							<p>Nottre politique libriste, se veux coller le plus possible au attente d'un web 3.0 ouvert sur le monde...</p>
+							<footer>
+								<a href="#" class="button special">More</a>
+							</footer>
+						</article>
+						<article>
+							<header>
+								<h3>Une équipe humaine<br />pour des projet humain</h3>
+							</header>
+							<p>RaiseMind se veux être la pour vous, non pour votre argent. Nous serons la de bout en bout pour votre projet !</p>
+							<footer>
+								<a href="#" class="button special">More</a>
+							</footer>
+						</article>
+						<article>
+							<header>
+								<h3>Nos service<br /> Des service simple est utile pour vous !</h3>
+							</header>
+							<p>Nous metons en place tout une suite d'outils pour facillit votre activité, et votre communication sur internet !</p>
+							<footer>
+								<a href="#" class="button special">More</a>
+							</footer>
+						</article>
+					</div>
+				</div>
+			</section>
+
+		<!-- Two -->
+			<section id="two" class="wrapper style1 special">
+				<div class="inner">
+					<header>
+						<h2>Notre équipe</h2>
+						<p>Notre petite équipe en image.</p>
+					</header>
+					<div class="flex flex-4">
+					<div class="box person">
+							<div class="image round">
+								<img src="images/pic03.jpg" alt="Person 1" />
+							</div>
+							<h3><a href="http://p.lezarius.tech/">Ronce</a></h3>
+							<p>Admin sys, Gestion d'équipe, fait un peux tout.</p>
+						</div>
+
+						</div>
+					</div>
+				</div>
+			</section>
+
+		<!-- Three -->
+			<section id="three" class="wrapper special">
+				<div class="inner">
+					<header class="align-center">
+						<h2>Vos projet</h2>
+						<p>Voici vos projet, que nous soutenons avec amours.</p>
+					</header>
+					<div class="flex flex-2">
+						<article>
+							<div class="image fit">
+								<img src="images/pic01.jpg" alt="Pic 01" />
+							</div>
+							<header>
+								<h3>Praesent placerat magna</h3>
+							</header>
+							<p>Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor lorem ipsum.</p>
+							<footer>
+								<a href="#" class="button special">More</a>
+							</footer>
+						</article>
+						<article>
+							<div class="image fit">
+								<img src="images/pic02.jpg" alt="Pic 02" />
+							</div>
+							<header>
+								<h3>Fusce pellentesque tempus</h3>
+							</header>
+							<p>Sed adipiscing ornare risus. Morbi est est, blandit sit amet, sagittis vel, euismod vel, velit. Pellentesque egestas sem. Suspendisse commodo ullamcorper magna non comodo sodales tempus.</p>
+							<footer>
+								<a href="#" class="button special">More</a>
+							</footer>
+						</article>
+					</div>
+				</div>
+			</section>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="inner">
+					<div class="flex">
+						<div class="copyright">
+							&copy; RaiseMind. Design: RaiseMind. Images: RaiseMind. Logo: <a href="https://twitter.com/SoniopArt">Soniop</a>.
+						</div>
+						<ul class="icons">
+							<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+						</ul>
+					</div>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
