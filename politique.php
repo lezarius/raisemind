7cc655c1f72c0625c2fd571c5ee97db54fe4fae8
lezarius <remy.mazabraud@gmail.com>
2017-06-19 12:48:50 +0200

    adding politique text

--- politique.php
+++ politique.php
@@ -24,8 +24,18 @@
 						<h2>Notre politique</h2>
 						<p>Une politique libriste et ouvert sur le monde, pour vous !</p>
 					</header>
-					<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
-					<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
+					<p>Notre politique se veux simple.
+Des projets, des service et des application à taille et prix humain.
+Aurevoire les projet de dev à plusieur millier d'euro. </p>
+					<p>Ici vous ne payer pas de license excésife.
+Uniquement les compétence utiliser sur le temps de travaille.</p>
+<p>Ici vos donné sont en lieux sur.
+Chiffré est notre mot d'ordre. La sécurité n'est pas qu'une chosse obscure comme nous laise pensez Macron.
+Chez nous elle est réalité. Votre réalité.</p>
+<p>Nous voulons votre bonheur, et votre sécurité pour cela tout nos outils sont open source/ sous license libre..
+Nous même contribuon à la vie et la bonne santé de ses même projets. </p>
+<p>Nous vous accompagnion de bout en bout dans votre projet.
+Avec énormément de data love.</p>
 
 				</div>
 			</section>
