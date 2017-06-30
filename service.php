595591bbc04a9a12c96169b32d5746709d05a3da
lezarius <remy.mazabraud@gmail.com>
2017-06-30 15:00:59 +0200

    add  €/unité and €/mois on diff servicing

--- service.php
+++ service.php
@@ -43,17 +43,17 @@
                   <tr>
                     <td>Site vitrine</td>
                     <td>Un petit site avec 2-3 pages et une page contact avec formulaire.</td>
-                    <td>240</td>
+                    <td>240 €/unité</td>
                   </tr>
                   <tr>
                     <td>Site vitrine & blog</td>
                     <td>Site avec 2-3 pages, une page contact et une partie blog.</td>
-                    <td>360</td>
+                    <td>360 €/unité</td>
                   </tr>
                   <tr>
                     <td>Blog</td>
                     <td>Un simple blog et une page contact, pour partager vos plus beaux billet.</td>
-                    <td>120</td>
+                    <td>120 €/unité</td>
                   </tr>
                   <tr>
                     <td>e-commerce</td>
@@ -79,34 +79,34 @@
                   <tr>
                     <th>Design</th>
                     <th>Tout ce qui tourne autour de la création graphique pour votre entreprise !</th>
-                    <th>Prix en euro</th>
+                    <th>Prix</th>
                   </tr>
                 </thead>
                 <tbody>
                   <tr>
                     <td>Template</td>
                     <td>Une template bootstrap sur mesure pour votre site vitrine (design web)</td>
-                    <td>190</td>
+                    <td>190 €/unité</td>
                   </tr>
                   <tr>
                     <td>Logo</td>
                     <td>Un logo pour vos sites web.</td>
-                    <td>110</td>
+                    <td>110 €/unité</td>
                   </tr>
                   <tr>
                     <td>Template mail</td>
                     <td>Nous intégrons votre logo et d'autres informations importantes pour vous dans un design unique pour vos mails !</td>
-                    <td>80</td>
+                    <td>80 €/unité</td>
                   </tr>
                   <tr>
                     <td>Papeterie</td>
                     <td>Carte de visite et entête de lettre. (À voir en devis pour l'impression de ses produits)</td>
-                    <td>80</td>
+                    <td>80 €/unité</td>
                   </tr>
                   <tr>
                     <td>Flyers</td>
-                    <td> Flyers sur mesure bons pour toutes vos campagnes de pub</td>
-                    <td>150</td>
+                    <td> Flyers sur mesure bons pour toutes vos campagnes de pub (À voir en devis pour l'impression de ses produits)</td>
+                    <td>150 €/unité</td>
                   </tr>
                 </tbody>
                 </table>
@@ -129,12 +129,12 @@
 										<tr>
 											<td>Hosting web 1</td>
 											<td>Pour héberger vos sites web à moyen trafic (jusqu'à 3000 visites uniques simultanées.)</td>
-											<td>10</td>
+											<td>10 €/mois</td>
 										</tr>
 										<tr>
 											<td>Hosting web 2</td>
 											<td>Pour héberger vos sites web à plus gros trafic (jusqu'à 6000 visites uniques simultanées.)</td>
-											<td>25</td>
+											<td>25 €/mois</td>
 										</tr>
 										<tr>
 											<td>Hosting web 3</td>
@@ -144,7 +144,7 @@
 										<tr>
 											<td>Piwik</td>
 											<td> Une alternative auto-héberger de vos statistiques de visites et autre. Les données recoltées ne sont vendues à aucun tiers.</td>
-											<td>10</td>
+											<td>10 €/mois</td>
 										</tr>
 									</tbody>
 									<tfoot>
