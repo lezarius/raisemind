9033e53706754cca3cff3d6fdf72fa3fc69711ea
lezarius <remy.mazabraud@gmail.com>
2017-09-20 16:04:36 +0200

    

--- contact.php
+++ contact.php
@@ -28,47 +28,7 @@
       <div class="container">
           <div class="row">
               <div class="col-md-8">
-                  <div class="well well-sm">
-                      <form>
-                      <div class="row">
-                          <div class="col-md-6">
-                              <div class="form-group">
-                                  <label for="name">
-                                      Nom</label>
-                                  <input type="text" class="form-control" id="name" placeholder="Enter name" required="required" />
-                              </div>
-                              <div class="form-group">
-                                  <label for="email">
-                                      Email Address</label>
-                                  <div class="input-group">
-                                      <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span>
-                                      </span>
-                                      <input type="email" class="form-control" id="email" placeholder="Enter email" required="required" /></div>
-                              </div>
-                              <div class="form-group">
-                                  <label for="subject">
-                                      Sujet</label>
-                                  <select id="subject" name="subject" class="form-control" required="required">
-                                      <option value="na" selected="">En choisir un:</option>
-                                      <option value="service">Service</option>
-                                      <option value="suggestions">Devis</option>
-                                      <option value="product">Autre (datalove, suggestion, etc)</option>
-                                  </select>
-                              </div>
-                          </div>
-                          <div class="col-md-6">
-                              <div class="form-group">
-                                  <label for="name">
-                                      Message</label>
-                                  <textarea name="message" id="message" class="form-control" rows="9" cols="25" required="required"
-                                      placeholder="C'est un monde avec des licornes. Oh, heureusement que ce texte va être remplacé."></textarea>
-                              </div>
-                              <button type="submit" class="btn btn-primary pull-right" id="btnContactUs">
-                                  Envoyer le message</button>
-                          </div>
-                      </div>
-                      </form>
-                  </div>
+                
               </div>
               <div class="col-md-4">
               <img src="images/logo200.png" alt=""></img>
